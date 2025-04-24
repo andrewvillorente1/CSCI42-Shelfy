6a7872315965dbc2e78fbca46317bd95df148aad
Zia Garcia <maigelazia.garcia@gmail.com>
2025-04-24 19:59:46 +0800

    updated urls

--- shelves/urls.py
+++ shelves/urls.py
@@ -7,7 +7,7 @@ app_name = 'shelves'
 urlpatterns = [
     path('', UserShelvesView.as_view(), name='user_shelves'),
     path('add/', AddToShelfView.as_view(), name='add_to_shelf'),
-    path('<str:username>/<int:shelf_id>', PublicShelfView.as_view(), name='public_shelf'),
-    path('<str:username>/<int:shelf_id>/<int:item_id>/', ShelfItemDetailView.as_view(), name='shelf_detail'),
-    path('<int:shelf_id>/remove/<int:item_id>/', RemoveFromShelfView.as_view(), name='remove_from_shelf'),
+    path('remove/', RemoveFromShelfView.as_view(), name='remove_from_shelf'),
+    path('<str:username>/', PublicShelfView.as_view(), name='public_shelf'),
+    path('<str:username>/<int:item_id>/', ShelfItemDetailView.as_view(), name='shelf_detail'),
 ]
